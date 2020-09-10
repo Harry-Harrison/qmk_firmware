eb12937c4d10618f6802bb6ee15800d202b076d8
Harry Harrison <h@rryharrison.co>
2020-09-11 00:03:52 +0100

    Add missing commas

--- keyboards/lily58/keymaps/Harry-Harrison/keymap.c
+++ keyboards/lily58/keymaps/Harry-Harrison/keymap.c
@@ -41,10 +41,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                   `----------------------------'           '------''--------------------'
  */
  [_QWERTY] = LAYOUT( \
-  KC_1,     KC_2,   KC_3,    KC_4,    KC_5,    KC_6,                     KC_7,    KC_8,    KC_9,    KC_MINUS,    KC_EQUAL \
+  KC_1,     KC_2,   KC_3,    KC_4,    KC_5,    KC_6,                     KC_7,    KC_8,    KC_9,    KC_MINUS,    KC_EQUAL, \
   KC_GESC,  KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
   KC_TAB,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-  KC_LSPO,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, MO(_LOWER),    MO(_RAISE),KC_N,KC_M, KC_COMM, KC_DOT,  KC_SLSH,  KC_RSPC, \
+  KC_LSPO,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, MO(_LOWER),    MO(_RAISE), KC_N, KC_M, KC_COMM, KC_DOT,  KC_SLSH,  KC_RSPC, \
                         KC_LCTRL, KC_LALT, KC_LGUI, KC_SPC, KC_ENT, KC_RGUI, KC_LBRC, KC_RBRC \
 ),
 
@@ -64,7 +64,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LOWER] = LAYOUT( \
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12, \
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX,XXXXXXX, XXXXXXX, KC_DELETE,\
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_DELETE,\
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, KC_MUTE, KC_VOLD, KC_VOLU, KC_TILD, KC_GRAVE, \
   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, XXXXXXX, XXXXXXX, KC_MPLY, KC_MPRV, KC_MNXT, KC_BSLASH, KC_PIPE, \
                             KC_LCTRL, KC_LALT, KC_LGUI, MO(_BACKLIGHT), KC_ENT, KC_RGUI, KC_BRID, KC_BRIU \
@@ -88,7 +88,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_P1,    KC_P2,      KC_P3,  KC_P4,      KC_P5,      KC_P6,                      KC_P7,   KC_P8,  KC_P9,  KC_P0,  KC_MINUS,   KC_EQUAL, \
   KC_CAPS,   XXXXXXX,    KC_UP,   XXXXXXX,    XXXXXXX,    XXXXXXX,           XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
   KC_NLCK,  KC_LEFT,    KC_DOWN,   KC_RGHT,   KC_F5,   KC_F6,                 XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, \
-  KC_LSHIFT,   XXXXXXX,   XXXXXXX,   XXXXXXX,  XXXXXXX,  XXXXXXX,XXXXXXX,   _______, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, KC_RSHIFT \
+  KC_LSHIFT,   XXXXXXX,   XXXXXXX,   XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX,   _______, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, KC_RSHIFT, \
                             KC_LCTRL, KC_LALT, KC_LGUI, KC_SPC, KC_ENT, KC_RGUI, KC_LBRC, KC_RBRC \
 ),
 
