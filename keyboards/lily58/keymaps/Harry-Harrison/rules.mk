828c226b7f831672f5409b7d5a7fa81816366de4
Harry Harrison <h@rryharrison.co>
2020-09-24 08:58:14 +0100

    Add custom lily58 map

--- keyboards/lily58/keymaps/Harry-Harrison/rules.mk
+++ keyboards/lily58/keymaps/Harry-Harrison/rules.mk
@@ -13,7 +13,7 @@ MIDI_ENABLE = no            # MIDI controls
 AUDIO_ENABLE = no           # Audio output on port C6
 UNICODE_ENABLE = no         # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
-RGBLIGHT_ENABLE = no       # Enable WS2812 RGB underlight.
+RGBLIGHT_ENABLE = yes       # Enable WS2812 RGB underlight.
 SWAP_HANDS_ENABLE = no      # Enable one-hand typing
 OLED_DRIVER_ENABLE= yes     # OLED display
 WPM_ENABLE = yes
@@ -26,8 +26,11 @@ SRC +=  ./lib/rgb_state_reader.c \
         ./lib/layer_state_reader.c \
         ./lib/logo_reader.c \
         ./lib/keylogger.c \
-        # ./lib/mode_icon_reader.c \
-        # ./lib/host_led_state_reader.c \
-        # ./lib/timelogger.c \
+        ./lib/mode_icon_reader.c \
+        ./lib/host_led_state_reader.c \
+        ./lib/timelogger.c \
 
 BOOTLOADER = atmel-dfu
+
+Link_Time_Optimization = yes # Reduce size of firmware by optimizing at link time
+EXTRAFLAGS += -flto
